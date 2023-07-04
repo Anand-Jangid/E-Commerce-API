7e9ee39c89425f66ea8bba1d6d721aa916877aae
Anand-Jangid <jangidme88@gmail.com>
2023-07-04 11:12:28 +0530

    Initial Project

---
+++ controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require('../models/users');
+const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors')
+const {createJWT} = require("../utils");
+
+const register = async (req, res) => {
+    const {email, name, password} = req.body; 
+    const emailAlreadyExists = await User.findOne({email}); 
+    if(emailAlreadyExists){
+        throw new CustomError.BadRequestError('Email Already Exists')
+    }
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount ? 'admin' : 'user';
+    const user =  await User.create({name, email, password, role});
+    const tokenUser = {name: user.name, userId: user._id, role: user.role};
+    const token = createJWT({payload: tokenUser});
+    const oneDay = 1000*60*60*24;
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay)
+    })
+    res.status(StatusCodes.CREATED).json({user: tokenUser});
+}
+
+const login = async (req, res) => {
+    res.send('login user');
+}
+
+const logout = async (req, res) => {
+    res.send('logout user');
+}
+
+module.exports = {register, login, logout};
