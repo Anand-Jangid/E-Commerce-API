7a8b547add28b78525742222b7a9838aca3733c6
Anand-Jangid <jangidme88@gmail.com>
2023-07-04 12:32:25 +0530

    Login route setup completed

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,7 +15,9 @@ const attachCookiesToResponse = ({res, user}) => {
 
     res.cookie('token', token, {
         httpOnly: true,
-        expiresIn: new Date(Date.now() + oneDay)
+        expiresIn: new Date(Date.now() + oneDay),
+        secure: process.env.NODE_ENV === 'production',
+        signed: true
     });
 }
 
